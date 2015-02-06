1387242728753328b4d5a5f9aa34e3bf18eb0c94
David Maran <david@figcreative.co.uk>
2015-02-06 14:26:41 +0000

    Add kitchen sink

--- grunt/concat.js
+++ grunt/concat.js
@@ -1,14 +1,14 @@
 module.exports = {
   dist: {
-    src: [
-	    'js/*.js', 
+    src: [ 
 	    'bower_components/jquery/dist/jquery.js', 
 	    'bower_components/modernizr/modernizr.js', 
 	    'bower_components/fastclick/lib/fastclick.js', 
 	    'bower_components/html5shiv/dist/html5shiv.js',
 	    'bower_components/lazyloadxt/dist/jquery.lazyloadxt.js', 
 	    'bower_components/responsive-nav/responsive-nav.js',
-	    'bower_components/ResponsiveSlides.js/responsiveslides.js'
+	    'bower_components/ResponsiveSlides.js/responsiveslides.js',
+	    'js/*.js'
 	    ],
     dest: 'html/assets/js/build.js',
   }
---
+++ js/script.js
@@ -0,0 +1,36 @@
+    var nav = responsiveNav(".nav-collapse", { // Selector
+        animate: true, // Boolean: Use CSS3 transitions, true or false
+        transition: 284, // Integer: Speed of the transition, in milliseconds
+        label: "Menu", // String: Label for the navigation toggle
+        insert: "before", // String: Insert the toggle before or after the navigation
+        customToggle: "", // Selector: Specify the ID of a custom toggle
+        closeOnNavClick: false, // Boolean: Close the navigation when one of the links are clicked
+        openPos: "relative", // String: Position of the opened nav, relative or static
+        navClass: "nav-collapse", // String: Default CSS class. If changed, you need to edit the CSS too!
+        navActiveClass: "js-nav-active", // String: Class that is added to <html> element when nav is active
+        jsClass: "js", // String: 'JS enabled' class which is added to <html> element
+        init: function(){}, // Function: Init callback
+        open: function(){}, // Function: Open callback
+        close: function(){} // Function: Close callback
+    });
+
+$(function() {
+    $(".rslides").responsiveSlides({
+		  auto: true,             // Boolean: Animate automatically, true or false
+		  speed: 500,            // Integer: Speed of the transition, in milliseconds
+		  timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
+		  pager: false,           // Boolean: Show pager, true or false
+		  nav: false,             // Boolean: Show navigation, true or false
+		  random: false,          // Boolean: Randomize the order of the slides, true or false
+		  pause: false,           // Boolean: Pause on hover, true or false
+		  pauseControls: true,    // Boolean: Pause when hovering controls, true or false
+		  prevText: "Previous",   // String: Text for the "previous" button
+		  nextText: "Next",       // String: Text for the "next" button
+		  maxwidth: "",           // Integer: Max-width of the slideshow, in pixels
+		  navContainer: "",       // Selector: Where controls should be appended to, default is after the 'ul'
+		  manualControls: "",     // Selector: Declare custom pager navigation
+		  namespace: "rslides",   // String: Change the default namespace used
+		  before: function(){},   // Function: Before callback
+		  after: function(){}     // Function: After callback
+});
+});
